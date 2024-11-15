9e266581afb64fed8a6b8025e7cbb774b1a74717
Rollocraft <Rollocraft117@gmail.com>
2024-11-15 06:53:08 +0100

    fixed smt for the 33. time

--- server/app.js
+++ server/app.js
@@ -75,7 +75,7 @@ app.get('/providers/:region', (req, res) => {
   res.send(providers[req.params.region]);
 });
 
-app.post('/text', (req, res) => {
+app.get('/text', (req, res) => {
   if (req.body.getcarriers != null
       && (req.body.getcarriers === '1'
        || req.body.getcarriers.toLowerCase() === 'true')) {
@@ -98,6 +98,10 @@ app.post('/canada', (req, res) => {
 
 app.post('/intl', (req, res) => {
   textRequestHandler(req, res, stripPhone(req.body.number), req.body.carrier, 'intl');
+});
+
+app.post('/de', (req, res) => {
+  textRequestHandler(req, res, stripPhone(req.body.number), req.body.carrier, 'de');
 });
 
 // Start server
